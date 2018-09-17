859303f42c5ffc5b29b828f3ed28247ea2946220
Natali Vakheta <nata.vakheta@gmail.com>
2018-09-17 15:27:21 +0300

    fix module-09

--- js/module-08/index.js
+++ js/module-08/index.js
@@ -1,58 +1,54 @@
 
-class Gallery {
-  constructor(data) {
-    this._parentNode = data.parentNode;
+let min = 0;
+let sec = 0;
+let msec = 0;
 
-    data.items.forEach(item => this.addImage(data.parentNode, item));
-
-    const fullview = data.parentNode.querySelector('.fullview');
-    fullview.innerHTML = "";
-    const img = document.createElement("img");
-    const image = data.items[data.defaultActiveItem-1];
-    img.src = image.fullview;
-    img.alt = image.alt;
-    fullview.appendChild(img);
+function start() {
+  if (document.querySelector('.btn.js-start').innerHTML == 'Start' || document.querySelector('.btn.js-start').innerHTML == 'Continue') {
+    return;
   }
-
-  addImage(parentNode, image) {
-    const li = document.createElement("li");
-    const inParentNode = parentNode;
-    const img = document.createElement("img");
-    img.src = image.preview;
-    img.alt = image.alt;
-    img.setAttribute("data-fullview", image.fullview);
-    li.appendChild(img);
-    inParentNode.querySelector('.preview').appendChild(li);
+  msec += 100;
+  if (msec >= 1000) {
+    sec += 1;
+    msec = 0;
   }
+  if (sec >= 60) {
+    sec = 0;
+    min += 1;
+  }
+  const timeString = (min < 10 ? '0'+min : min)+':'+(sec < 10 ? '0'+sec : sec)+'.'+msec;
+  document.querySelector('.time.js-time').innerHTML = timeString;
+  setTimeout(start, 100);
 }
 
-function handleImageClick(event) {
-  event.preventDefault();
-  const target = event.target;
-  if (target.nodeName !== "IMG") return;
-  const fullview = document.querySelector('.image-gallery').querySelector('.fullview');
-  fullview.innerHTML = "";
-  const img = document.createElement("img");
-  img.src = target.getAttribute("data-fullview");
-  img.alt = target.alt;
-  fullview.appendChild(img);
-}
-document.querySelector('.image-gallery').querySelector(".preview").addEventListener("click", handleImageClick);
-
-const galleryItems = [
-  { preview: 'image/download.jpeg', fullview: 'image/downloadfiles.jpeg', alt: "alt text 1" },
-  { preview: 'image/forestway.jpeg', fullview: 'image/forestwayfull.jpeg', alt: "alt text 2" },
-  { preview: 'image/images.jpeg', fullview: 'image/naturefull.jpg', alt: "alt text 3" },
-  { preview: 'image/most-beatf.jpeg', fullview: 'image/outum.jpeg', alt: "alt text 4" },
-  { preview: 'image/river.jpeg', fullview: 'image/Beauty.jpg', alt: "alt text 5" },
-  { preview: 'image/tree.jpeg', fullview: 'image/sunfull.jpg', alt: "alt text 6" },
-];
-
-
-
-new Gallery({
-  items: galleryItems,
-  parentNode: document.querySelector('.image-gallery'),
-  defaultActiveItem: 1
+document.querySelector('.btn.js-start').addEventListener('click', () => {
+  console.log(event);
+  document.querySelector('.btn.js-reset').disabled = false;
+  document.querySelector('.btn.js-take-lap').disabled = false;
+  const obj = document.querySelector('.btn.js-start');
+  if (obj.innerHTML == "Start" || obj.innerHTML == "Continue") {
+    obj.innerHTML = "Pause";
+  } else if (obj.innerHTML == "Pause") {
+    obj.innerHTML = "Continue";
+  }
+  start();
 });
+
+document.querySelector('.btn.js-take-lap').addEventListener('click', (event) => {
+  const li = document.createElement("li");
+  li.textContent = document.querySelector('.time, .js-time').textContent;
+  document.querySelector('.laps.js-laps').appendChild(li);
+});
+
+document.querySelector('.btn.js-reset').addEventListener('click', (event) => {
+  event.path[0].disabled = true;
+  document.querySelector('.btn.js-take-lap').disabled = true;
+  document.querySelector('.laps.js-laps').innerHTML = '';
+  document.querySelector('.btn.js-start').innerHTML = 'Start';
+  document.querySelector('.time.js-time').innerHTML = '00:00.0';
+  min = 0;
+  sec = 0;
+  msec = 0;
+});
+
 
